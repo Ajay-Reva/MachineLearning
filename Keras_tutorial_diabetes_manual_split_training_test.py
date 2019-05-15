4ddafc90cb1df59300c8ce4b43545afe07a0301e
Balasubramaniam <ajay.balasubramaniam@thermofisher.com>
2019-05-15 15:48:18 +0200

    Manual and automatic splitting

--- Keras_tutorial_diabetes_manual_split_training_test.py
+++ Keras_tutorial_diabetes_manual_split_training_test.py
@@ -11,14 +11,15 @@ dataset = numpy.loadtxt("C:/Users/ajay.balasubramaniam/Desktop/pima-indians-diab
 
 
 #split input(X) and output(Y) from the dataset.
+  #Manual splitting
 X_train = dataset[0:600,0:8] #all rows (0-599) of the dataset, columns from 0-7 (8th is not included when written as 0:8).
 Y_train = dataset[0:600,8]   #all rows (0-599) of the dataset, only the last column that says if it is an onset of diabetes or not.
 X_test = dataset[600:768,0:8] #test data is split - the rest of the data apart from training set                                         split: ~ 78%(training) - 22%(test)
 Y_test = dataset[600:768,8] #test data is split - the rest of the data apart from training set
 
-
-X = dataset[:,0:8] #all rows of the dataset, columns from 0-7 (8th is not included when written as 0:8).
-Y = dataset[:,8]   #all rows of the dataset, only the last column that says if it is an onset of diabetes or not.
+  #Automatic splitting, for now the whole data is in X and Y, later in model.fit, validation_split takes care of automatic split. 
+#X = dataset[:,0:8] #all rows of the dataset, columns from 0-7 (8th is not included when written as 0:8).
+#Y = dataset[:,8]   #all rows of the dataset, only the last column that says if it is an onset of diabetes or not.
 
 #define the model.
 model = Sequential()
@@ -30,7 +31,10 @@ model.add(Dense(1, init='uniform', activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #fit the model.
-model.fit(X, Y, validation_split=0.33, nb_epoch=150, batch_size=10) #Epochs - number of times the network sees the entire training dataset, batch_size - number of samples after which the weights are updated.
+model.fit(X, Y, nb_epoch=150, batch_size=10) #Epochs - number of times the network sees the entire training dataset, batch_size - number of samples after which the weights are updated. 
+
+#Validation_split parameter for automatic splitting is used
+#model.fit(X, Y, validation_split=0.33, nb_epoch=150, batch_size=10) #Epochs - number of times the network sees the entire training dataset, batch_size - number of samples after which the weights are updated. 
 
 #evaluate the model.
 scores = model.evaluate(X_test,Y_test)
